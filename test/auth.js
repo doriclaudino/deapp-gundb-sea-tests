e662fbf542bb12bd03b9444300a49d5ec4520d6c
dori claudino <dori.claudino@gmail.com>
2019-07-06 23:14:06 -0400

    encrypt and decrypt scenarios

--- test/auth.js
+++ test/auth.js
@@ -3,6 +3,10 @@ let should = chai.should();
 let expect = chai.expect;
 let auth = require('../auth');
 
+var Gun = require('gun'); // in NodeJS 
+require('gun/sea');
+var SEA = Gun.SEA;
+
 const users = {
   boston: { username: 'boston', password: 'boston' },
   chicago: { username: 'chicago', password: 'chicago' },
@@ -56,3 +60,66 @@ describe('Auth', () => {
   }))
 });
 
+describe('Sea', () => {
+  describe('Encrypt and Decrypt', () => {
+    describe('Encrypt, Sign and Decrypt his own message', () => {
+      Object.keys(users).forEach((user => {
+        it(`User ${user} should be able to encrypt and decrypt his message`, async () => {
+          const boston = await auth.createOrAuthUser(users[user])
+          const bostonPair = boston.sea
+          const messageToEncrypt = `Good Morning ${user}!`
+          var enc = await SEA.encrypt(messageToEncrypt, bostonPair);
+          var data = await SEA.sign(enc, bostonPair);
+
+          //check if comming from user
+          var msg = await SEA.verify(data, bostonPair.pub);
+          var dec = await SEA.decrypt(msg, bostonPair);
+          dec.should.equal(messageToEncrypt)
+        });
+      }))
+    })
+
+    describe('Encrypt and Decrypt  another user message', () => {
+      Object.keys(users).forEach(((user, index, array) => {
+        const next = index === array.length - 1 ? array[0] : array[index + 1]
+        it(`User ${user} should be able to decrypt ${next} message`, async () => {
+          const boston = await auth.createOrAuthUser(users[next])
+          const chicago = await auth.createOrAuthUser(users[user])
+          const bostonPair = boston.sea
+          const chicagoPair = chicago.sea
+          const messageToEncrypt = `Good Morning ${next}!`
+          var enc = await SEA.encrypt(messageToEncrypt, await SEA.secret(chicagoPair.epub, bostonPair));
+          var dec = await SEA.decrypt(enc, await SEA.secret(bostonPair.epub, chicagoPair));
+          dec.should.equal(messageToEncrypt)
+        });
+      }))
+    })
+
+
+    describe('Encrypt, Sign and Decrypt another user message', () => {
+      Object.keys(users).forEach(((user, index, array) => {
+        const next = index === array.length - 1 ? array[0] : array[index + 1]
+        it(`User ${user} should be able to decrypt ${next} message`, async () => {
+          const boston = await auth.createOrAuthUser(users[next])
+          const chicago = await auth.createOrAuthUser(users[user])
+          const bostonPair = boston.sea
+          const chicagoPair = chicago.sea
+          const messageToEncrypt = `Good Morning ${next}!`
+          var enc = await SEA.encrypt(messageToEncrypt, await SEA.secret(chicagoPair.epub, bostonPair));
+
+          var data = await SEA.sign(enc, bostonPair);
+
+          //check if comming from boston
+          var msg = await SEA.verify(data, bostonPair.pub);
+
+          var dec = await SEA.decrypt(msg, await SEA.secret(bostonPair.epub, chicagoPair));
+          dec.should.equal(messageToEncrypt)
+        });
+      }))
+    })
+
+    /**
+     * handle not share informations
+     */
+  });
+});
